95a0465e4425a672a9d324e1a425969179b59b48
maxim palekhin <infp40@gmail.com>
2021-06-21 21:44:09 +0300

    Create minor comment in ViewController

--- ChiefBook/StoryBoards/ViewControllers/MainListViewController.swift
+++ ChiefBook/StoryBoards/ViewControllers/MainListViewController.swift
@@ -12,6 +12,7 @@ class MainListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // minorComment from Max
     }
 
 

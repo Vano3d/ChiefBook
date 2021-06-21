35c4d0645a8a3e266ad48b897a7cde6c32606bf6
Ivan Ch <vano3d@gmail.com>
2021-06-21 22:15:50 +0300

    Ivan's comment

--- ChiefBook/ViewControllers/MainListViewController.swift
+++ ChiefBook/ViewControllers/MainListViewController.swift
@@ -13,6 +13,7 @@ class MainListViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // minorComment from Max
+        // minorComment from Ivan
     }
 
 

c5628cd0f3bdc030c386d3aed6970d0f2530fe0a
Ivan Ch <vano3d@gmail.com>
2021-06-21 20:04:31 +0300

    save

---
+++ ChiefBook/Model/RecipeDetails.swift
@@ -0,0 +1,12 @@
+//
+//  RecipeDetails.swift
+//  ChiefBook
+//
+//  Created by Chupinsky Ivan on 21.06.2021.
+//
+
+struct Recipe {
+    var title: String
+    var subTitle: String
+    var description: String
+}
---
+++ ChiefBook/StoryBoards/ViewControllers/RecipeDetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ChiefBook
+//
+//  Created by Chupinsky Ivan on 21.06.2021.
+//
+
+import UIKit
+
+class RecipeDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}

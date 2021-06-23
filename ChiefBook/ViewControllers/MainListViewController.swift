4f4f60c50497267ebd9ffce2771e63a35972a987
Ivan Ch <vano3d@gmail.com>
2021-06-23 21:09:14 +0300

    save absolutely new

--- ChiefBook/ViewControllers/MainListViewController.swift
+++ ChiefBook/ViewControllers/MainListViewController.swift
@@ -8,44 +8,15 @@
 import UIKit
 
 class MainListViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-
-    private var recipeList = Recipe.getRecipeList()
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.delegate = self
-        tableView.dataSource = self
         
     }
 
 }
 
 
-extension MainListViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: "recipeCell",
-            for: indexPath
-        )
-        let recipe = recipeList[indexPath.row]
-        
-        var content = cell.defaultContentConfiguration()
-        
-        content.text = recipe.title
-        content.secondaryText = recipe.difficulty
-        
-        cell.contentConfiguration = content
-        
-        return cell
-    }
 
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        recipeList.count
-    }
-}
 

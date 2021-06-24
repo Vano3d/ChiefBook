8619e5d8f77d60c9a491d1c44cd85a454911a808
Ivan Ch <vano3d@gmail.com>
2021-06-24 03:02:18 +0300

    Всё работает - и удаление, и перемещение.

--- ChiefBook/ViewControllers/MainListViewController.swift
+++ ChiefBook/ViewControllers/MainListViewController.swift
@@ -7,17 +7,15 @@
 
 import UIKit
 
-class MainListViewController: UIViewController {
+class MainListViewController: UITableViewController {
 
 
-    @IBOutlet weak var tableView: UITableView!
+
     
     private var recipeList = Recipe.getRecipeList()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
         
         navigationItem.leftBarButtonItem = editButtonItem
         
@@ -25,18 +23,13 @@ class MainListViewController: UIViewController {
 
 }
 
-extension MainListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print("Ты тапнул на меня!")
-    }
-}
 
-extension MainListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension MainListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         recipeList.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt
+    override func tableView(_ tableView: UITableView, cellForRowAt
                 indexPath: IndexPath) -> UITableViewCell {
                 let cell = tableView.dequeueReusableCell(
                     withIdentifier: "cell",
@@ -56,7 +49,7 @@ extension MainListViewController: UITableViewDataSource {
     }
     
 }
-// Передаём данные на экран с подробным опианием рецепта
+// Передаём данные на экран с подробным описанием рецепта
 extension MainListViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let vc = segue.destination as? RecipeDetailsViewController,
@@ -71,27 +64,35 @@ extension MainListViewController {
 
 
 extension MainListViewController {
-    
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+    // отображает нужную иконку слева если тапнуть edit. Например, ".delete" = иконка удаления
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        print("123")
+    // удаляет ячейку
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        recipeList.remove(at: indexPath.row)
+       
+        tableView.deleteRows(at: [indexPath], with: .fade)
+        
     }
+
     
     
-    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
         false
     }
     
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+    
+    // метод удаляет и вставляет ячейку, которую схватили и двигают
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         
         let currentRecipe = recipeList.remove(at: sourceIndexPath.row)
         recipeList.insert(currentRecipe, at: destinationIndexPath.row)
     }
     
-    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+    //Отображает штучки справа, которые можно потянуть
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
